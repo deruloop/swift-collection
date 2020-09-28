3b204ccb49bba17c07d62dd3467dd77f5dcc436d
Cristiano Calicchia <c.calicchia@icloud.com>
2020-09-28 19:34:38 +0200

    New Project: Moonshot (Errata Corrige)

---
+++ SwiftUI/Moonshot/Moonshot/Astronauts.swift
@@ -0,0 +1,14 @@
+//
+//  Astronauts.swift
+//  Moonshot
+//
+//  Created by Cristiano Calicchia on 28/09/2020.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ SwiftUI/Moonshot/Moonshot/Mission.swift
@@ -0,0 +1,39 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Cristiano Calicchia on 28/09/2020.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        if let launchDate = launchDate {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .long
+            return formatter.string(from: launchDate)
+        } else {
+            return "N/A"
+        }
+    }
+    
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRole]
+    let description: String
+}
